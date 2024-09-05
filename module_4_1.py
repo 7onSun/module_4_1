7383b7e9852e7bae39cb373fd4bc67afa2d34275
7onSun <7onsun@mail.ru>
2024-09-05 09:40:32 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as faked
+from true_math import divide as trued
+
+result1 = faked(69, 3)
+result2 = faked(3, 0)
+result3 = trued(49, 7)
+result4 = trued(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
